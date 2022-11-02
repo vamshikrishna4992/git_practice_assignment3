ddf01e4513097d5bdc571eac11be08f701613fb5
krishna668 <narigekumar9@gmail.com>
2022-11-02 12:17:56 +0530

    added the prime code

---
+++ prime.js
@@ -0,0 +1,18 @@
+let count = 0;
+for(let i=1;i<=num;i++){
+if(num%i==0){
+count++;
+}
+}
+if(count<=2){
+return true;
+}
+return false;
+}
+}
+let a = checkPrime(13);
+if(a==true){
+console.log("Prime);
+}else{
+console.log("Not A Prime");
+}
